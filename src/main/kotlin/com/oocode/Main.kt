a6bcf4beb55566a001b49df66fba2a4379cf47ad
Ivan Moore <ivan@teamoptimization.com>
2022-12-07 18:35:50 +0000

    with another forecaster

--- src/main/kotlin/com/oocode/Main.kt
+++ src/main/kotlin/com/oocode/Main.kt
@@ -1,4 +1,5 @@
 import com.teamoptimization.ForecasterClient
+import com.teamoptimization.ForecastingClient
 import com.teamoptimization.LocatorClient
 import java.time.DayOfWeek
 
@@ -7,6 +8,7 @@ fun main(args: Array<String>) {
         throw RuntimeException("Must specify Day and Place")
     }
     forecast(args[0], args[1])
+    forecast2(args[0], args[1])
 }
 
 fun forecast(day: String, place: String) {
@@ -15,6 +17,15 @@ fun forecast(day: String, place: String) {
     val location = LocatorClient().locationOf(place)
     val (minTemp, maxTemp, description) = forecaster.forecast(dayNumber, location.lat, location.long)
     println("forecaster: $place day=$day min=$minTemp max=$maxTemp description=$description")
+}
+
+fun forecast2(day: String, place: String) {
+    val forecasting = ForecastingClient()
+    val dayOfWeek = DayOfWeek.valueOf(day.uppercase())
+    val minTemp = forecasting.min(dayOfWeek, place)
+    val maxTemp = forecasting.max(dayOfWeek, place)
+    val description = forecasting.desc(dayOfWeek, place)
+    println("forecasting: $place day=$day min=$minTemp max=$maxTemp description=$description")
 }
 
 fun moo() = "boo"
