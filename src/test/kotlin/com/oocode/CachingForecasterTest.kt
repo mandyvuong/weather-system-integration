2d6e3a9531bd41bd50404640212c681adcd5ad95
Amanda Vuong <mandyvuong@gmail.com>
2023-01-12 12:00:35 +0000

    AV, WD get forecast and should be able to return multiple forecast

--- src/test/kotlin/com/oocode/CachingForecasterTest.kt
+++ src/test/kotlin/com/oocode/CachingForecasterTest.kt
@@ -8,16 +8,28 @@ import java.time.DayOfWeek
 internal class CachingForecasterTest {
 
     @Test
-    fun `get forecast`() {
+    fun `get forecast and return a forecast`() {
         val delegate = FakeForecaster()
         val forecaster = CachingForecaster(delegate)
         assertThat(forecaster.getForecast(DayOfWeek.FRIDAY, "London"), equalTo(Forecast(11, 22, "Cloudy")))
+    }
+
+    @Test
+    fun `get forecast and should be able to return multiple forecast`() {
+        val delegate = FakeForecaster()
+        val forecaster = CachingForecaster(delegate)
+        assertThat(forecaster.getForecast(DayOfWeek.FRIDAY, "London"), equalTo(Forecast(11, 22, "Cloudy")))
+        assertThat(forecaster.getForecast(DayOfWeek.THURSDAY, "NY"), equalTo(Forecast(10, 12, "Snowy")))
     }
 }
 
 class FakeForecaster : Forecaster {
     override fun getForecast(day: DayOfWeek, location: String): Forecast {
-        return Forecast(11, 22, "Cloudy")
+        if (day == DayOfWeek.FRIDAY && location == "London") {
+            return Forecast(11, 22, "Cloudy")
+        } else {
+            return Forecast(10, 12, "Snowy")
+        }
     }
 
 }
