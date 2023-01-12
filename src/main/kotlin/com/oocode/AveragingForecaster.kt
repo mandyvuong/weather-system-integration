45ccbec03b80cd1d1864bdcf99ab3075d0c59e92
Amanda Vuong <mandyvuong@gmail.com>
2023-01-12 16:00:00 +0000

    AV WD DC WL Pedantic formatting

--- src/main/kotlin/com/oocode/AveragingForecaster.kt
+++ src/main/kotlin/com/oocode/AveragingForecaster.kt
@@ -2,14 +2,13 @@ package com.oocode
 
 import java.time.DayOfWeek
 
-class AveragingForecaster(val f1: Forecaster, val f2: Forecaster): Forecaster {
+class AveragingForecaster(val f1: Forecaster, val f2: Forecaster) : Forecaster {
     override fun getForecast(day: DayOfWeek, location: String): Forecast {
         val f1Forecast = f1.getForecast(day, location)
         val f2Forecast = f2.getForecast(day, location)
-        val minAverage = (f1Forecast.min+f2Forecast.min)/2
-        val maxAverage = (f1Forecast.max+f2Forecast.max)/2
+        val minAverage = (f1Forecast.min + f2Forecast.min) / 2
+        val maxAverage = (f1Forecast.max + f2Forecast.max) / 2
 
-        return Forecast(min=minAverage, max=maxAverage, f1Forecast.description)
+        return Forecast(min = minAverage, max = maxAverage, f1Forecast.description)
     }
-
 }
