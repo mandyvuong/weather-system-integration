bad1d0da66b0d27b5cd12e5176d9d5870cb220a2
Amanda Vuong <mandyvuong@gmail.com>
2023-01-12 15:58:22 +0000

    AV WD DC WL Calculate averaging forecaster.

--- src/main/kotlin/com/oocode/AveragingForecaster.kt
+++ src/main/kotlin/com/oocode/AveragingForecaster.kt
@@ -4,7 +4,12 @@ import java.time.DayOfWeek
 
 class AveragingForecaster(val f1: Forecaster, val f2: Forecaster): Forecaster {
     override fun getForecast(day: DayOfWeek, location: String): Forecast {
-        return f1.getForecast(day, location)
+        val f1Forecast = f1.getForecast(day, location)
+        val f2Forecast = f2.getForecast(day, location)
+        val minAverage = (f1Forecast.min+f2Forecast.min)/2
+        val maxAverage = (f1Forecast.max+f2Forecast.max)/2
+
+        return Forecast(min=minAverage, max=maxAverage, f1Forecast.description)
     }
 
 }
--- src/test/kotlin/com/oocode/AveragingForecasterTest.kt
+++ src/test/kotlin/com/oocode/AveragingForecasterTest.kt
@@ -7,12 +7,18 @@ import java.time.DayOfWeek
 
 internal class AveragingForecasterTest {
     @Test
-    fun `get min and max temp and return average`() {
-        val f1 = FakeForecaster()
-        val f2 = FakeForecaster()
+    fun `get min and max temp and return average from different forecasters`() {
+        val f1 = FakeForecaster(Forecast(20, 30, "Cloudy"))
+        val f2 = FakeForecaster(Forecast(10, 20, "Snowy"))
         val forecaster = AveragingForecaster(f1, f2)
-        
-        assertThat(forecaster.getForecast(DayOfWeek.FRIDAY, "London"), equalTo(Forecast(11, 22, "Cloudy")))
+
+        assertThat(forecaster.getForecast(DayOfWeek.FRIDAY, "London"), equalTo(Forecast(15, 25, "Cloudy")))
     }
 
+    class FakeForecaster(val forecast: Forecast) : Forecaster {
+        override fun getForecast(day: DayOfWeek, location: String): Forecast {
+            return forecast
+        }
+
+    }
 }
